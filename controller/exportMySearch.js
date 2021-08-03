e8d17c7ca4d93fc5382efeea8461a28dd631529a
trung <duonggiatrung113@gmail.com>
2021-08-03 15:58:27 +0700

    thay doi href

--- controller/exportMySearch.js
+++ controller/exportMySearch.js
@@ -8,17 +8,17 @@ function mySearch() {
     }
     else if (str.search(/iphone/i) != -1) {
         document.getElementById("title-modal").innerHTML = search;
-        document.getElementById("body-modal").innerHTML = `<div class='card m-auto'><a href='./iPhone/iPhone.html' onclick='pick(1)'><div class='card-body text-center'><img src='./image/shopdunkiphone.jpg' alt=''><h4>iPhone </h4></div> </a></div>;`
+        document.getElementById("body-modal").innerHTML = `<div class='card m-auto'><a href='./iPhone.html' onclick='pick(1)'><div class='card-body text-center'><img src='./image/shopdunkiphone.jpg' alt=''><h4>iPhone </h4></div> </a></div>;`
         
     }
     else if (str.search(/ipad/i) != -1) {
         document.getElementById("title-modal").innerHTML = search;
-        document.getElementById("body-modal").innerHTML = "<div class='card m-auto'><a href='./iPhone/iPhone.html' onclick='pick(2)'><div class='card-body text-center'><img src='./image/ipad3.jpg' alt=''><h4>iPad </h4></div> </a></div>";
+        document.getElementById("body-modal").innerHTML = "<div class='card m-auto'><a href='./iPhone.html' onclick='pick(2)'><div class='card-body text-center'><img src='./image/ipad3.jpg' alt=''><h4>iPad </h4></div> </a></div>";
         
     }
     else if (str.search(/mac/i) != -1) {
         document.getElementById("title-modal").innerHTML = search;
-        document.getElementById("body-modal").innerHTML = "<div class='card m-auto'><a href='./iPhone/iPhone.html' onclick='pick(3)'><div class='card-body text-center'><img src='./image/shopdunkmac.jpg' alt=''><h4>MacBook </h4></div> </a></div>";
+        document.getElementById("body-modal").innerHTML = "<div class='card m-auto'><a href='./iPhone.html' onclick='pick(3)'><div class='card-body text-center'><img src='./image/shopdunkmac.jpg' alt=''><h4>MacBook </h4></div> </a></div>";
         
     }
     else {
